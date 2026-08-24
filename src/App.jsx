4fdc029f5990b521da32c2a281995c5938bfb384
BryanCodeDev <tiagotroller021019@gmail.com>
2026-08-23 23:27:01 -0500

    feat: Actualizacion de diseño

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ const AppContent = () => {
   };
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-cream">
       <Navbar
         business={businessInfo}
       />
